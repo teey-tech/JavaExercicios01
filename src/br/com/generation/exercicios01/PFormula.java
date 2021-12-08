3a5e3728a223af2727898575761763381461ff04
teey-tech <thiagobatistadg@hotmail.com>
2021-12-08 15:49:38 -0300

    Primeiros Exercicios No Java 01

--- src/br/com/generation/exercicios01/PFormula.java
+++ src/br/com/generation/exercicios01/PFormula.java
@@ -6,7 +6,10 @@ public class PFormula {
 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-        double x1,x2, y1, y2, d;
+        double x1,x2, y1, y2;
+        double p1 ,p2, p3;
+        double d;
+        
         System.out.println("Digite o valor de x1: ");
         x1=leia.nextDouble();
         System.out.println("Digite o valor de x2: ");
@@ -15,10 +18,13 @@ public class PFormula {
         y1=leia.nextDouble();
         System.out.println("Digite o valor de y2: ");
         y2=leia.nextDouble();
-        leia.close();
-        d=Math.sqrt(((Math.pow(x2-x1, 2))+(Math.pow(y2-y1, 2))));
+       
+        p1 = Math.pow((x2-x1), 2);
+        p2 = Math.pow((y2-y1), 2);
+        p3 = p1 + p2;
+        d = Math.sqrt(p3);
         System.out.println("A distancia entre eles é: "+d);
-
+        leia.close();
 	}
 
 }
